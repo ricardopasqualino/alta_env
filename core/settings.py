6de2946a2532403eb9d833e4565ab59a33c898fc
Ricardo <ricardo.pasqualino@gmail.com>
2025-05-05 21:55:58 -0300

    debug

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['alta-env.onrender.com', 'localhost', '127.0.0.1']
 
