0c1cdcc36ba81aed654811738e8c447ef4dfed66
Ricardo <ricardo.pasqualino@gmail.com>
2025-06-18 08:03:12 -0300

    ajuste memoria no gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,14 +1,14 @@
 import multiprocessing
 import os
 
-# Número de workers
-workers = multiprocessing.cpu_count() * 2 + 1
+# Número de workers - reduzindo para um número mais conservador
+workers = 2  # Reduzindo o número de workers
 
 # Timeout
 timeout = 300  # 5 minutos
 
-# Limites de memória
-max_requests = 1000
+# Limites de memória - mais conservadores
+max_requests = 500  # Reduzindo o número máximo de requisições
 max_requests_jitter = 50
 
 # Configurações de timeout
@@ -21,9 +21,9 @@ errorlog = '-'
 loglevel = 'info'
 
 # Configurações de worker
-worker_class = 'gthread'  # Usando worker thread ao invés de uvicorn
-threads = 4
-worker_connections = 1000
+worker_class = 'gthread'
+threads = 2  # Reduzindo o número de threads
+worker_connections = 500  # Reduzindo conexões por worker
 
 # Configurações de segurança
 limit_request_line = 4094
@@ -31,4 +31,8 @@ limit_request_fields = 100
 limit_request_field_size = 8190
 
 # Configurações de memória
-worker_tmp_dir = '/dev/shm' 
+worker_tmp_dir = '/dev/shm'
+
+# Configurações adicionais para controle de memória
+max_worker_lifetime = 3600  # Reiniciar workers após 1 hora
+max_worker_lifetime_jitter = 60  # Adicionar variação aleatória ao tempo de vida 
